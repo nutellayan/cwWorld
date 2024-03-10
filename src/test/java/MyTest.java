c855810964348db04fd5ce37046667b9499edf54
nutellayen <157748332+nutellayan@users.noreply.github.com>
2024-03-10 17:41:11 +0000

    Amended MyTest

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,12 +1,10 @@
-package com.napier.sem;
-
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MyTest {
 
     @Test
-    void unitTest() {
+    public void unitTest() {
         assertEquals(5, 5);
     }
 }
